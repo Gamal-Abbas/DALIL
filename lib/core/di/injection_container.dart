15d3a94cb68df6607169b21dc55b39a195b550b0
Gamal Abbas <engineer.gamal.abbas@gmail.com>
2026-04-14 19:21:53 +0200

    Architect: Setup Clean Architecture folders and Firebase initialization

---
+++ lib/core/di/injection_container.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance; // sl stands for Service Locator
+
+Future<void> init() async {
+  // ! Features - (e.g., Auth, Landmarks, etc.)
+  // Data sources
+  
+  // Repositories
+  
+  // Use cases
+  
+  // Blocs / Providers / Cubits
+
+  // ! Core
+  // Network, Local Storage, etc.
+
+  // ! External
+  // Shared Preferences, HTTP client, Dio, Firebase, etc.
+}
